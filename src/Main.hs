eb13654bca2b7e96bc14dc66d7972e1635bb6a20
furu <ba09219@gmail.com>
2016-01-28 03:14:30 +0900

    Parsec の消費概念

--- src/Main.hs
+++ src/Main.hs
@@ -8,12 +8,22 @@ number = do
   ds <- many1 digit
   return (read ds)
 
+-- Parsec では、パースが成功し消費された入力データは忘れ去られる。
+-- try で囲むと入力データを覚えていて、パースが失敗した場合は
+-- 入力を巻き戻してくれる。
+--
+-- 以下のパーサで "256" をパースすると
+-- z <- number で 256 が消費され、次に + を見つけようとするが
+-- + はないのでパースに失敗する。この部分は try で囲まれているので
+-- 256 が巻き戻されて、次の number でパースされ、これには成功する。
+-- よって、以下のパーサは "256+256" も "256" もパースできる。
+-- という理解でいいのかな。
 calc :: Parser Integer
 calc = do
-  x <- number
-  char '+'
+  x <- try (do z <- number; char '+'; return z)
   y <- number
   return $ x + y
+  <|> number
 
 run :: String -> String
 run input = case parse calc "Test" input of
